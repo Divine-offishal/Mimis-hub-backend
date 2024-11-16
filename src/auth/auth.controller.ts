5e8527a02233bc138adf2f4ebd89a0748fad7d7c
Divine Udise <divineoffishal@gmail.com>
2024-11-16 07:29:02 +0100

    chores: Add authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { LocalGuard } from './guards/local.guard';
 import { Request } from 'express';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Post('login')
   @UseGuards(LocalGuard)
   loginUser(@Req() req: Request) {
     return req.user;
+  }
+
+  @Post('register')
+  @UsePipes(new ValidationPipe())
+  registerUser(@Body() authPayloadDto: AuthPayloadDto) {
+    return this.authService.createUser(authPayloadDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -14,7 +14,7 @@ export class AuthService {
   ) {}
 
   async validateUser({ email, password }: AuthPayloadDto) {
-    const findUser = await this.UserModel.findOne({ email });
+    const findUser = await this.UserModel.findOne({ email }).lean();
 
     if (!findUser) return null;
 
@@ -25,6 +25,28 @@ export class AuthService {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password: _, ...user } = findUser;
 
-    return this.jwtService.sign(user);
+    const token = this.jwtService.sign(user);
+
+    return { ...user, access_token: token };
+  }
+
+  async createUser(authPayloadDto: AuthPayloadDto): Promise<User> {
+    try {
+      const { email, password } = authPayloadDto;
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+      const newUser = new this.UserModel({
+        email: email,
+        password: hashedPassword,
+      });
+
+      return await newUser.save();
+    } catch (error) {
+      if (error.code == 11000)
+        throw new BadRequestException('A user with that email already exists.');
+
+      throw error;
+    }
   }
 }
